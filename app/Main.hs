07b97fa8f6fd3fedde576de09d964c203458c514
stouma915 <sasakitouma@yahoo.ne.jp>
2023-02-28 11:25:38 +0900

    Update Main

--- app/Main.hs
+++ app/Main.hs
@@ -4,10 +4,42 @@ import System.Environment
 
 import ArgumentParse
 
+version :: String
+version = "1.0.0"
+
+showUsage :: IO ()
+showUsage = do
+  progName <- getProgName
+  putStrLn   "Usage:"
+  putStrLn $ "   " ++ progName ++ " [OPTIONS] [SOURCE]"
+
+showHelp :: IO ()
+showHelp = do
+  putStrLn $ "bf-compiler-hs v" ++ version
+  putStrLn   ""
+  showUsage
+  putStrLn   ""
+  putStrLn   "Options:"
+  putStrLn   "   -h, --help    print help information."
+  putStrLn   ""
+  putStrLn   "Args:"
+  putStrLn   "   <SOURCE>      Brainf**k source file."
+
+showUnknownArgError :: String -> IO ()
+showUnknownArgError arg = do
+  putStrLn $ "Found argument '" ++ arg ++ "' which wasn't expected, or isn't valid in this context."
+  putStrLn   ""
+  showUsage
+  putStrLn   ""
+  putStrLn   "For more information try --help"
+
 main :: IO ()
 main = do
   runMode <- ioParseArgsAndComputeRunMode getArgs
   case runMode of
-    (ShowHelpMode)                -> putStrLn "ShowHelp"
-    (RunCompilerMode srcFilePath) -> putStrLn $ "RunCompiler " ++ srcFilePath
-    (ShowUnknownArgErrorMode arg) -> putStrLn $ "UnknownArg " ++ arg
+    (ShowHelpMode) ->
+      showHelp
+    (RunCompilerMode srcFilePath) ->
+      putStrLn $ "RunCompiler " ++ srcFilePath
+    (ShowUnknownArgErrorMode arg) ->
+      showUnknownArgError arg
